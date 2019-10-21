d41185e6163e001c32379e0e08d5470cf492bfd5
Marina Khokkonen <mariskasl@mail.ru>
2019-10-21 11:43:46 +0300

    5. Личный проект: начинаем программировать

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+const init = () => {
+  ReactDOM.render(
+      <App />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
 
